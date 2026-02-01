61882014fe9d959d34797d83a80e4c854f6b02a5
P. Patrick Socha <sochap@gmail.com>
2026-02-01 18:26:07 +0100

    Fixing rebase issues

--- py/circuitpy_defns.mk
+++ py/circuitpy_defns.mk
@@ -671,7 +671,6 @@ $(filter $(SRC_PATTERNS), \
 	wifi/Packet.c \
 	wifi/PowerManagement.c \
 	rm690b0/__init__.c \
-	rm690b0/image_converter.c \
 	rm690b0_lvgl/__init__.c \
 	rm690b0_lvgl/RM690B0_LVGL.c \
 	rm690b0_lvgl/Widget.c \
@@ -985,11 +984,6 @@ ifneq ($(filter 1,$(CIRCUITPY_JPEGIO) $(CIRCUITPY_RM690B0)),)
 SRC_MOD += lib/tjpgd/src/tjpgd.c
 $(BUILD)/lib/tjpgd/src/tjpgd.o: CFLAGS += -Wno-shadow -Wno-cast-align
 CFLAGS_MOD += -I$(TOP)/lib/tjpgd/src
-endif
-
-ifneq ($(filter 1,$(CIRCUITPY_JPEGIO) $(CIRCUITPY_RM690B0)),)
-SRC_MOD += lib/esp_jpeg/src/esp_jpeg.c
-CFLAGS_MOD += -I$(TOP)/lib/tjpgd/src -I$(TOP)/lib/esp_jpeg/include
 endif
 
 ifeq ($(CIRCUITPY_HASHLIB_MBEDTLS_ONLY),1)
